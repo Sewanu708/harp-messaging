914bd68f6121ce16e4ba486e6fc54dc52e8fa1a5
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-29 19:00:17 +0100

    refactor: simplify BarChart component and remove unused ChartContainer

--- src/components/Dashboard/BarChart.tsx
+++ src/components/Dashboard/BarChart.tsx
@@ -1,28 +1,19 @@
 'use client'
-import { BarChart, Bar } from "recharts";
-import { ChartContainer,  } from "../ui/chart"
-
-
+import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 interface BarChartProps {
-    data: { month: string, data: number }[]
+    data: { month: string; data: number }[];
 }
 
-const chartConfig = {
-    data: {
-        label: 'Data',
-        color: '#0F6C68',
-    }
-}
 export default function Barchart({ data }: BarChartProps) {
     return (
-
-        <ChartContainer config={chartConfig} className="w-full h-60 bg-white p-4">
-            <BarChart data={data} width={0} height={0} style={{ width: '100%', height: '100%' }}>
-            <Bar dataKey="data" fill={chartConfig.data.color} />
-            
-            </BarChart>
-        </ChartContainer>
-
-    )
+        <div className="w-full h-60 bg-white p-4 rounded-md ">
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={data}>
+                    <XAxis dataKey="month" stroke="#888888" />
+                    <Tooltip />
+                    <Bar dataKey="data" fill="#0F6C68" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>)
 }

7232da39ca7d10a6aa7ef3ef3f1fd5550ea05838
Sewanu Isaiah <sewanu708@gmail.com>
2025-07-06 19:44:40 +0100

    deployment issue

--- src/app/email/domain/api/route.ts
+++ src/app/email/domain/api/route.ts
@@ -10,18 +10,3 @@ export async function GET():Promise<NextResponse> {
         },2000)
     })
 }
-
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const index = domainTableData.findIndex(domain => domain.id === params.id)
-
-  if (index === -1) {
-    return NextResponse.json({ error: "Domain not found" }, { status: 404 })
-  }
-
-  domainTableData.splice(index, 1)
-
-  return NextResponse.json({ message: "Domain deleted successfully" }, { status: 200 })
-}

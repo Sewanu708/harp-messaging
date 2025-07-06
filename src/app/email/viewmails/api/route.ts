665bedbd4422396588f0bc30cc76da41b0773d21
Sewanu Isaiah <sewanu708@gmail.com>
2025-07-06 20:03:56 +0100

    Promise

--- src/app/email/viewmails/api/route.ts
+++ src/app/email/viewmails/api/route.ts
@@ -1,7 +1,7 @@
 import { newEmailData } from "@/data";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(): Promise<NextResponse> {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(

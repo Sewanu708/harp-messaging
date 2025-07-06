3ffcbc4d3aa5a88c3c8cea9180f7e9b46b1f4a05
Sewanu Isaiah <sewanu708@gmail.com>
2025-07-06 19:59:27 +0100

    Promise

--- src/app/email/viewmails/[id]/route.ts
+++ src/app/email/viewmails/[id]/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server"
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const mail = newEmailData.find(d => d.messageId === params.id)
+  const {id} = await params
+  const mail = newEmailData.find(d => d.messageId === id)
 
   if (!mail) {
     return NextResponse.json({ error: 'Mail not found' }, { status: 404 })
